57d07fc4336e0594ec4733eefb059cc1384457bf
Rodolfo Rafael Cetina <rodolfo.cetina@clubhub.ai>
2025-08-14 12:50:17 -0600

    fix: update config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://example.com',
+	server:{
+		port: 3000,
+		host: true
+	}
+});
