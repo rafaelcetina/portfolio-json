63c9c785e1191919431a2f8453b7e718463f2856
Rodolfo Rafael Cetina <rodolfo.cetina@clubhub.ai>
2025-09-07 00:36:20 -0600

    feat: add sitemap integration and update dependencies in configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://rafaelcetina.com",
+  integrations: [sitemap()]
 });
